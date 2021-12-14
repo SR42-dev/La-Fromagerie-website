7a70d51c9246ec98b7475b76d5bc7d4bca263ba7
Sriram Radhakrishna <83068519+SR42-dev@users.noreply.github.com>
2021-12-14 13:36:09 +0530

    Add files via upload

---
+++ source/backend/models/Users.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+const UserSchema = new Schema({
+    Firstname:{
+        type: String,
+        required: true
+    },
+    Email:{
+        type: String,
+        required: true,
+    },
+    Username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    MobileNo:{
+        type: Number,
+        required: true
+    },
+    Password:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+const User = mongoose.model('Users', UserSchema)
+// User.createIndexes()
+module.exports = User
